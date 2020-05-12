a77af789a900cd4dde89156ad367b1d11024e353
Christopher Burdette <subdue1914@gmail.com>
2020-05-11 20:56:39 -0400

    added javascript and report card page
    Also updated index page.

---
+++ assets/js/message.js
@@ -0,0 +1,33 @@
+var AGrade = [
+    'You have shown excellent ability to set a budget and stick with it. Keep up the good work!',
+    'You manage your money maturely. Way to go!',
+    'You excel at creating a budget. Woohoo!',
+    'You have done a great job facing and sticking to your planned budget. Please continue to nurture and encourage this behavior over the next month!',
+    'You have shown that you are responsibile with your budget. Excellent!',
+    'You are able to successfully stick to your budget. You Rock!',
+    'You have a good knowledge of budgeting. Woohoo!',
+    'You understand the concept of making a budget and sticking to it. Be proud of yourself!'
+ 
+]
+
+
+var CGrade = [
+    'You need to pay more attention to your budget. Try decreasing your monthly budget planned.',
+    'You are making progress, but there is work to be done. Stick to the amount you have planned.',
+    'Needs improvement. Try re-adjusting your budget in this category.',
+
+ 
+]
+
+
+var FGrade = [
+    'This is not acceptable! You need to improve your budgeting for this category. Try increasing your monthly budget planned!',
+    'You need to show more appropriate spending behavior. Get it together!',
+    'You are having considerable difficulty with your budget. Please increase your monthly budget planned!',
+    'You are having trouble with your budgeting. You can improve your grade by not overspending!',
+    'You need to really focus on your budget. More improvement is needed in this budget category. If possible, try using cash instead of a card.',
+    'Not Good! You are encouraged to spend more time adjusting your monthly budget.'
+ 
+]
+   
+
